b84751f26d9f0f6e7ec8c08d6e9d4cd18a435ab4
Sara Wilson <sarakatewilson98@gmail.com>
2020-11-21 18:39:52 -0600

    adding password array of characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,9 @@
 // Assignment Code
+//starter code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+//starter code
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -9,6 +11,17 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// starting personalized code
+
+function generatePassword(){
+
+}
+// the acceptable password criteria(array)
+var upperCase= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ['1','2','3','4','5','6','7','8','9']
+var special = ["\"","!","#","$","%","&","\'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","`","{","|","}","~"];
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
