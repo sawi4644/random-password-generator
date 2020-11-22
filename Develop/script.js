1479f44133849faca565f47c19aea47c0731f364
Sara Wilson <sarakatewilson98@gmail.com>
2020-11-21 19:34:30 -0600

    adding alert windows for user to pick conditions

--- Develop/script.js
+++ Develop/script.js
@@ -15,13 +15,23 @@ function writePassword() {
 
 function generatePassword(){
 
-}
+var pickedChar = [];
+// asking user password criteria
+var upper = confirm("Use uppercase letters?");
+var lower = confirm("Use lowercase letters?");
+var special = confirm("Use special characters?");
+var number = confirm("Use numbers?");
+
+
+
 // the acceptable password criteria(array)
 var upperCase= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = ['1','2','3','4','5','6','7','8','9']
+var numbers = ['1','2','3','4','5','6','7','8','9'];
 var special = ["\"","!","#","$","%","&","\'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","`","{","|","}","~"];
 
+
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
